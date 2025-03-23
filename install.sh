78442e9d4adc58acbca1b747818b7dd6c4bdc191
milossramek <msramek22@gmail.com>
2025-03-23 11:35:01 +0100

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,30 @@
+#! /bin/bash
 #stript to simplify installation
-#Assumes that we run on a Debian-like system, using 'apt' to install things using sudo
+#Assumes that we run on a Debian-like system, using 'apt' to install things
+#Default ocr language is set in CoverUP.py to Slovak (slk). It can be changed by setting the COVERUP_OCR_LANG environment variable
 
 env_dir=coverup_venv
+pdfanon=/usr/local/bin/pdfanon
 
-sudo apt install python3-venv
+sudo apt install python3-venv -y
+sudo apt install tk python3-tk -y
+sudo apt install tesseract-ocr -y
+languages="slk ces deu fra"
+for lang in $languages; do sudo apt install tesseract-ocr-$lang -y; done
 
 rm -rf $env_dir 
+sudo rm -f $pdfanon
 
 #Create virtual environment
 #/usr/bin/python3 -m venv coverup_venv
-/usr/bin/python -m venv $env_dir
+/usr/bin/python3 -m venv $env_dir
 
 source $env_dir/bin/activate
 
 pip install -r requirements.txt
 
 # Create the pdfanon script in /usr/local/bin
-sudo echo "
+echo "
 #!/bin/bash
 
 if [ ! -n \"\$1\" ]
@@ -29,5 +37,10 @@ pdf=\$1
 path=$PWD
 source \$path/$env_dir/bin/activate
 \$path/CoverUP.py \$pdf
-" > /usr/local/bin/pdfanon
+" | sudo tee $pdfanon > /dev/null
+
+sudo chmod 755 $pdfanon
+
+echo
+echo "$pdfanon has been installed"
 
